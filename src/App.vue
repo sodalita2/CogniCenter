27aec3bdc202d28985ed5b0bd3a26ffc7573aa2a
Gabriel <gabriel.bitten59@gmail.com>
2022-09-14 20:15:31 -0300

    Feat(App): Setup inicial

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+
+
+</script>
+
+
+
+<template>
+  <header class="h-[100px] w-full bg-[#6FCF97]">
+
+  </header>
+
+  <RouterView />
+</template>
+
+
+
+
+
+
+
+
+<style scoped>
+
+</style>
