d4219f815e80e11f9b5a7a3043921c69dc1c2a8c
Gabriel <gabriel.bitten59@gmail.com>
2022-10-07 18:49:37 -0300

    Feat(Corrida): Esqueleto pronto

--- src/views/CorridaView.vue
+++ src/views/CorridaView.vue
@@ -1,30 +1,117 @@
 <script setup>
 import { ref } from 'vue';
 import BotaoRetornar from '../components/BotaoRetornar.vue';
+import $ from 'jquery';
 
-function StartGame(){
+const Cores = 3;
+const PosicoesCorrida = 7;
+// 0 => policia , 1 => bombeiro , 2 => ambulancia
+const PlayerColor = ref("");
 
+const ActualState = ref("Start");
+
+function StartGame(e){
+    PlayerColor.value = $(e.currentTarget).attr("id");
+    ActualState.value = "Jogando";
 }
 function EndGame(){
     
 }
 
+const Blacks = ref(0);
+const Reds = ref(0);
+const Whites = ref(0);
+function GenerateColor(){
+    let MaxNotInclusive = 3;
+    let ChosenColor = Math.floor(Math.random() * MaxNotInclusive);
+    switch(ChosenColor){
+        case 0:
+            if (Blacks.value == 6){
+                return GenerateColor();
+            }else{
+                Blacks.value += 1;
+                return "black";
+            }
+        case 1:
+            if (Reds.value == 6){
+                return GenerateColor();
+            }else{
+                Reds.value += 1;
+                return "red";
+            }
+        case 2:
+            if (Whites.value == 6){
+                return GenerateColor();
+            }else{
+                Whites.value += 1;
+                return "white";
+            }
+    }
+}
 
 
+var Columns = 3;
+var Rows = 6;
+var DashboardArray = [];
+
+for (let i=0;i<Columns;i++){
+    let RowFilled = [];
+    for (let j=0;j<Rows;j++){
+        RowFilled.push(GenerateColor());
+    }
+    DashboardArray.push(RowFilled);
+}
+
+console.log(DashboardArray);
 
 </script>    
 
 <template>
     <BotaoRetornar/>
-    <!-- Game Stats -->
-    <div class="h-[70px] w-full bg-[#91f58c] flex flex-row justify-center items-center">
-        <button v-if="ActualState == `Start`" @click="StartGame" class="h-[45px] w-[150px] ml-12 bg-[#F9F9F9] font-bold border-2">Iniciar Jogo</button>
-        <button v-else-if="ActualState == `Jogando`" @click="StopGame" class="h-[45px] w-[150px] ml-12 bg-[#F9F9F9] font-bold border-2">Parar Jogo</button>
-        <button v-else-if="ActualState == `Fim`" @click="RestartGame" class="h-[45px] w-[150px] ml-12 bg-[#F9F9F9] font-bold border-2">Recomeçar</button>
-    </div>
 
     <!-- Game Container -->
     <div class="flex-1 w-full flex flex-col justify-center items-center">
-        
+        <div v-if="ActualState == `Start`" class="h-[600px] w-[70%] flex flex-col justify-center items-center border-2 border-black">
+            <div class="text-3xl">Escolha um para apostar</div>
+            <div class="h-[90%] w-full flex flex-row justify-center items-center">
+                <!-- Policia Box -->
+                <div class="h-[60%] w-[25%] flex flex-col border-4 border-black rounded-3xl cursor-pointer" id="policia" @click="StartGame">
+                    <div class="flex items-center justify-center text-[2vw] font-bold h-[20%] w-full">Policia</div>
+                    <div class="h-[80%] w-full">
+                        <img class="h-full w-full object-fill rounded-b-3xl" src="../assets/Corrida/policia.png">
+                    </div>
+                </div>
+                <!-- Bombeiro Box -->
+                <div class="h-[60%] w-[25%] flex flex-col border-4 border-black rounded-3xl ml-12 cursor-pointer" id="bombeiro" @click="StartGame">
+                    <div class="flex items-center justify-center text-[2vw] font-bold h-[20%] w-full">Bombeiro</div>
+                    <div class="h-[80%] w-full">
+                        <img class="h-full w-full object-fill rounded-b-3xl" src="../assets/Corrida/bombeiro.png">
+                    </div>
+                </div>
+                <!-- Ambulancia Box -->
+                <div class="h-[60%] w-[25%] flex flex-col border-4 border-black rounded-3xl ml-12 cursor-pointer" id="ambulancia" @click="StartGame">
+                    <div class="flex items-center justify-center text-[2vw] font-bold h-[20%] w-full">Ambulancia</div>
+                    <div class="h-[80%] w-full">
+                        <img class="h-full w-full object-fill rounded-b-3xl" src="../assets/Corrida/ambulancia.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div v-if="ActualState == `Jogando`" class="h-full w-full">
+            <!-- Dashboard Div -->
+            <div class="h-[75%] w-full">
+                <div v-for="x in DashboardArray" class="h-1/3 w-full flex flex-row">
+                    <div v-for="y in x" class="h-full w-1/6 border-2 border-black">
+                        {{y}}
+                    </div>
+                </div>
+            </div>
+            <!-- Corrida Div -->
+            <div class="h-[25%] w-full flex flex-col justify-center items-center">
+                <div v-for="Carro in Cores" class="h-[60px] w-[70%] flex flex-row border-2 border-black">
+                    <div v-for="Posicao in PosicoesCorrida" class="h-full w-[15%] border-2 border-[red]"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
